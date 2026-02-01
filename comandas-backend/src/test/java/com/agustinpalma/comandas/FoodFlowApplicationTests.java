b1f1d0178930ed18466bfc050202f4817c583b21
Agustin Palma <aguspalqui@hotmail.com>
2026-02-01 15:18:32 -0300

    Initial commit

---
+++ comandas-backend/src/test/java/com/agustinpalma/comandas/FoodFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agustinpalma.comandas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
